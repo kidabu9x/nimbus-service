70a8ecc1b1d6f500a9defc70a8d9504cb720b236
Duong Nguyen <duongnk.techkids@gmail.com>
2020-03-29 17:39:17 +0700

    Update ApiController.java

--- api/src/main/java/vn/com/nimbus/api/controller/ApiController.java
+++ api/src/main/java/vn/com/nimbus/api/controller/ApiController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
     @GetMapping("/hello-world")
     public String helloWorld() {
-        return "Hello World!!!";
+        return "Hello World 22222!!!";
     }
 }
