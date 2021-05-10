8cd7ba0c05a3a9a30c2d753bbd1ef4905f29990d
DUONGNK <duongnk@onemount.com>
2021-05-10 12:13:59 +0700

    remove sensitive

--- common/deploy/k8s/dev/deploy-dev.sh
+++
@@ -1,6 +0,0 @@
-kubectl delete job $JOB_NAME --ignore-not-found=true -n dev
-
-envsubst < config.$ENVIRONMENT.yml > k8s-config.yml && kubectl apply -f k8s-config.yml -n dev
-
-envsubst < migration-job.yml > k8s-migration.yml && kubectl apply -f k8s-migration.yml -n dev
-
